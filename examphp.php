504207835702d4f21b2ee798e3ee1f6443e41e90
Francois Grandsire <grandsiref@gmail.com>
2019-09-18 10:27:11 +0200

    voici mon précieux exam sur php

---
+++ examphp.php
@@ -0,0 +1,54 @@
+<?php 
+
+// $NUMBER_ARRAY=[80,60,95,15,45,78,65,32,120,8,2,45,110];
+
+// $nbStronger=0;
+
+// for ($i=0; $i<count($NUMBER_ARRAY); $i++) { 
+//     # code...
+//     $nbCourant=$NUMBER_ARRAY[$i];
+//     if ($nbCourant>$nbStronger) {
+//         $nbStronger=$nbCourant;    
+//     }
+//     var_dump($i);
+// }
+
+// var_dump($nbStronger);
+
+
+//Class permettant de trouver le plus grand nombre
+class ArrayManager {
+
+    // tableau par défault
+    const DEFAULT_ARRAY=[80,60,95,15,45,78,65,32,120,8,2,110];
+    private $nbStronger;
+    private $array;
+
+    //méthode magique du constructeur
+    public function __construct(){
+        $this->nbStronger = 0;
+        $this->array=self::DEFAULT_ARRAY;
+        
+    }
+
+    //méthode pour trouver le plus grand nombre
+    public function findStrongerNumber(){
+        for ($i=0; $i<count($this->array); $i++) { 
+
+            $nbCourant=$this->array[$i];
+            if ($nbCourant>$this->nbStronger) {
+                $this->nbStronger=$nbCourant;    
+            }
+            // var_dump($i);
+        }
+        return $this->nbStronger;
+    }
+    //méthode pour renvoyer a l'extèrieure de la classe le nombre le plus grand
+    public function getNbStronger(){
+        return $this->nbStronger;
+    }
+    //méthode pour recevoir de l'extèrieure un tableau de nombre pour remplacer le tableau par défault
+    public function setArray(array $array){
+        $this->array=$array;
+    }
+}
---
+++ index_.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'examphp.php';
+//tableau que l'on veut envoyer a l'objet
+$array_number=[20,3,5,14,25,63,21,45,58];
+//appel de l'objet ArrayManager
+$test=new ArrayManager();
+//on envoie le tableau que l'on veut dans la méthode setter
+$test->setArray($array_number);
+//on lmance la méthode pour trouver le plus grand nombre
+$test->findStrongerNumber();
+// on affiche le résultat avec la méthode get qui permet de récupérer une propriété a l'extèrieure de la class
+echo($test->getNbStronger());
